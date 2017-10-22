e3c9f422169261002493ca838df739f301c18e3b
Rey Cruz <reycruz@Reys-MBP.attlocal.net>
2017-10-22 00:06:35 -0700

    added start div and end div to HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,24 @@
 // alert("HELLO!")
+
+// Timer 
+var seconds = 120
+
+var timer = setInterval(function(){
+	seconds--
+  console.log(seconds)
+},1000) 	
+
+$("#timer").html("Time Remaining: " + timer)
+// Variables
+
+// Functions 
+
+// Scores
+
+// change HTML to reflect changes 
+
+// main process on click
+
+
+	$("#game_container").hide();
+	$("#end_container").hide();
