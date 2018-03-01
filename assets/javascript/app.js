b466dc7b2e93c6574453e6efa19f6315a7aad3c5
Rey Cruz <reycruz@Reys-MacBook-Pro.local>
2018-02-28 20:00:41 -0800

    app working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,169 @@
-// alert("HELLO!")
-
-// Array of correct answers 
-var answers 		= ['3' , 'Friends', 'Stranger Things', 'X-Men: Apocalypse', 'Beyonce', 'Simone Biles', 'Rio, Brazil', 'Fifth Harmony', 'Captain Jack', 'Suicide Squad']
-
-
-
-//user answers arrays 
-var userAnswers 	= [];
-
-// Scores 
-var correctCount 	= 0;
-var wrongCount 		= 0;
-var unassweredCount = 0;
-
-// time counter 
-var seconds 		= 120
-
-
-$(document).ready(function() {
-
-	// start the game with hidden Divs
-	$("#game_container").hide();
-	$("#end_container").hide();
-
-
-	$("#start_button").on("click", function(){
-
-		// Hide the start div from the user
-		$("#start_container").hide();
-
-
-		// Show the Game div
-		$("#game_container").show();
-
-		// timer();
-		// return;
-
-	});
-
-// ======================
-var timer = setInterval(function(){
-	seconds--;
-  // console.log(seconds)
-},1000) 	
-
-$("#timer-counter").html("Time Remaining: " + seconds);
-
-
-
-// TIMER WORKS IF YOU CONSOLE LOG - HOWEVER, IT DOESNT DISPLAY ON THE SCREEN
-// ======================
-
-
-
-
-// =====================
-// WHEN SUBMIT BUTTON IS CLICK IS SHOULD DISPLAY THE END_CONTAINER WHICH SHOWS ALL THE ASNWERED, UNANSWERED, AND TOTAL SCORE
-// If the user answered all questions before the timer runs out 
-$("#submit").on("click", function(){
-
-	seconds = 0;
-	$("#game_container").hide();
-	$("#end_container").show();
-
+var panel = $("#quiz-area");
+// Question set
+var questions = [{
+  question: "How many dragons does Daenerys Targaryen have in the show Game of Thrones?",
+  answers: ["2", "3", "4", "5"],
+  correctAnswer: "3"
+}, {
+  question: "Which popular 1990s TV series that ran from 1994-2004 featured characters named Rachel, Monica, Phoebe, Joey, Chandler, and Ross?",
+  answers: ["Will and Grace", "Two and a Half Men", "Friends", "The Office"],
+  correctAnswer: "Friends"
+}, {
+  question: "Which Netflix Original series has a character named Eleven?",
+  answers: ["The Ranch", "OA", "The Defenders", "Stranger Things"],
+  correctAnswer: "Stranger Things"
+}, {
+  question: "Which X-Men film was released in 2016?",
+  answers: ["X-Men: Days of Future Past", "X-Men: First Class", "X-Men: The Last Stand", "X-Men: Apocalypse"],
+  correctAnswer: "X-Men: Apocalypse"
+}, {
+  question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
+  answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
+  correctAnswer: "The Lion King"
+}, {
+  question: "Who does Johnny Depp play in the Pirates of the Caribbean films?",
+  answers: ["Captain Jack Sparrow", "Will Turner", "Joshamee Gibbs", "Captain Armando"],
+  correctAnswer: "Captain Jack Sparrow"
+}, {
+  question: "Deadshot, Harley Quinn and Rick Flag are all characters in which 2016 film",
+  answers: ["Batman Vs Superman", "Star Trek Beyond", "Suicide Squad", "Captain America: Civil War"],
+  correctAnswer: "Suicide Squad"
+}, {
+  question: "What is Drake's last name",
+  answers: ["Graham", "Brooks", "Carter", "Walter"],
+  correctAnswer: "Graham"
+}, {
+    question: "Emma Watson is known for playing which character in Harry Potter?",
+    answers: ["Arabella Figg", "Hermione Granger", "Minerva McGonagall", "Dolores Umbridge"],
+    correctAnswer: "Hermione Granger"
+}, {
+    question: "Bruce Banner turns into what fictional superhero when he becomes angry?",
+    answers: ["Superman", "SpiderMan", "Iron Man", "The Hulk"],
+    correctAnswer: "The Hulk"
+  },];
+// Variable that will hold the setInterval
+var timer;
+var game = {
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
+    $("#start").remove();
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+    panel.append("<button id='done'>Done</button>");
+  },
+  done: function() {
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-8']:checked"), function() {
+        if ($(this).val() === questions[8].correctAnswer) {
+          game.correct++;
+        }
+        else {
+          game.incorrect++;
+        }
+      });
+      $.each($("input[name='question-9']:checked"), function() {
+        if ($(this).val() === questions[9].correctAnswer) {
+          game.correct++;
+        }
+        else {
+          game.incorrect++;
+        }
+      });
+    this.result();
+  },
+  result: function() {
+    clearInterval(timer);
+    $("#sub-wrapper h2").remove();
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+// CLICK EVENTS
+$(document).on("click", "#start", function() {
+  game.start();
 });
-
-// The timer runs out before user can finish the game 
-if(seconds === -1){
-
-
-	timeUp();
-
-
-	// hides the q&a div from user when the time runs out 
-	$("#game_container").hide();
-	console.log(hide);
-}
-
-
-// EACH QUESTION SHOULD BE CHECKED IF ITS CORRECT OR NOT
-// AT THE END OF THE GAME THE DIV WITH THE CORRECT ANSWERS, UNCORRECT ANSWERS, AND TOTAL SCORE SHOULD BE DISPLAYED 
-// THE GAME SHOULD RESTART AFTER EACH GAME - MAYBE ADD A RESTART BUTTON 
-
-
-
+$(document).on("click", "#done", function() {
+  game.done();
 });
-
